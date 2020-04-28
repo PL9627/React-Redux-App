f1976451dff627269a74f081f7b5a8ea8717ee62
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-28 15:40:42 -0700

    imported reducer into index.js and created store

--- app/src/index.js
+++ app/src/index.js
@@ -7,6 +7,10 @@ import {createStore, applyMiddleware} from 'redux';
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
+import reducer from './reducers/reducer';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
